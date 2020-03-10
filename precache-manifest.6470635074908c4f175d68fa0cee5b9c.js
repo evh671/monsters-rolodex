5779d32870a31b34b5e3c4b038e0ce1e1f5eea7e
evh671 <eyalnahom@gmail.com>
2020-03-10 12:55:30 +0200

    Updates

---
+++ precache-manifest.6470635074908c4f175d68fa0cee5b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4cfc49692261dd53b8f5e456e7cf0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9947c54f1d058768df7d",
+    "url": "/monsters-rolodex/static/css/main.71deaccd.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9947c54f1d058768df7d",
+    "url": "/monsters-rolodex/static/js/main.9a25f1b1.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
